9013774f9092f708501da96877a676a2b4599298
Javier Barrios <jbarrios.javier@gmail.com>
2021-04-17 14:21:43 -0400

    creation register Activity :construction:

--- app/src/main/java/com/android/testalphametrica/MainActivity.java
+++ app/src/main/java/com/android/testalphametrica/MainActivity.java
@@ -4,16 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static String CLASS_TAG=MainActivity.class.getSimpleName();
+    public Button sign_up;
+    public TextView login;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sign_up=findViewById(R.id.button_sign_up);
+        login=findViewById(R.id.text_view_login);
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 
