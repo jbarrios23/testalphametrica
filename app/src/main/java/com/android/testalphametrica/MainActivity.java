75b3eba4f82c7f26f2d5f2d984cb0e1414518309
Javier Barrios <jbarrios.javier@gmail.com>
2021-04-17 12:27:08 -0400

    creation of image folders and layaout folders :rocket:

--- app/src/main/java/com/android/testalphametrica/MainActivity.java
+++ app/src/main/java/com/android/testalphametrica/MainActivity.java
@@ -2,13 +2,20 @@ package com.android.testalphametrica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String CLASS_TAG=MainActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
